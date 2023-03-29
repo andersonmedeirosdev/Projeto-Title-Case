c0c405133887610cad10530c72a52fa48217d927
Anderson Medeiros <andersonviniciusmedeiros97@gmail.com>
2023-03-29 11:49:12 -0300

    Add project files.

---
+++ projetoTitleCase/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Xml;
+
+namespace projetoTitleCase
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string texto = "";
+            Console.WriteLine("Digite o texto que deseja modificar: ");
+            texto = Console.ReadLine();
+
+            string textoMinusculo = texto.ToLower();
+            string[] textoDigitado = textoMinusculo.Split(" ");
+
+            for (int i = 0; i < textoDigitado.Length; i++)
+            {
+                char[] primeiraLetra = textoDigitado[i].ToCharArray();
+                Console.Write(primeiraLetra[0].ToString().ToUpper() + textoDigitado[i].Substring(1));
+                Console.Write(" ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
